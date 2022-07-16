9b307780e3c26060f4001b6470f327200b5945b6
Zubair Irfan Khan <zubair.khan@airlifttech.com>
2022-07-16 13:27:54 +0500

    uploading to github

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Zubair Irfan Khan on 14/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
